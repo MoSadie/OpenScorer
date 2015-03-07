1e24dcf1958de6c7c2ccb944040cb225f6eb8fd6
MoSadie <MoSadie@users.noreply.github.com>
2015-03-06 16:38:38 -0800

    Fixed bugs

--- OpenScorer/src/OpenScorer/OpenScorer.java
+++ OpenScorer/src/OpenScorer/OpenScorer.java
@@ -1,20 +1,35 @@
 package OpenScorer;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
+
 import javax.swing.*;
 
-public class OpenScorer extends JFrame {
+public class OpenScorer  extends JFrame {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 5631011118025460373L;
-	File folder = new File("sheets");
-	File[] sheets = folder.listFiles();
-	public HashMap<String, Object> Items = new HashMap<String, Object>();
-	for (int i = 0; i < folder.listFiles().size(); i++)
-	{
+	public static void main (String[] arguments) {
+		File folder = new File("OpenScorerData");
+		if (folder.mkdir()) {
+			System.out.println("sheets folder created");
+		} else {
+			System.out.println("sheets folder not created");
+		}
+		File[] sheets = folder.listFiles();
+		HashMap<String, File> Items = new HashMap<String, File>();
+		for (File item : sheets) {
+			System.out.println("Name of file: " + item.toString());
+			Items.put(item.toString(), item);
+		}
+	}
+	public OpenScorer() {
+		super("OpenScorer");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(350,100);
+		setVisible(true);
 		
-		System.out.println("Name of file: " + sheets[i].toString());
 	}
 }
